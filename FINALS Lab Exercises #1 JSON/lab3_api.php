7b08c6212af46a6dce16c827d2df1c34335f94fe
dorigjohndel <dorigjohndel_bsit@plmun.edu.ph>
2026-09-16 18:49:53 +0800

    Update lab3_api.php

--- FINALS Lab Exercises #1 JSON/lab3_api.php
+++ FINALS Lab Exercises #1 JSON/lab3_api.php
@@ -1,1 +1,12 @@
+<?php
+header('Content-Type: application/json');
 
+$userProfile = [
+    "id" => 1,
+    "name" => "Dorig, Johndel B.",
+    "email" => "dorigjohndel9@gmail.com",
+    "status" => "active"
+];
+
+echo json_encode($userProfile);
+?>
