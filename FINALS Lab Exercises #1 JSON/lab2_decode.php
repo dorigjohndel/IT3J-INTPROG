4cc2fa434d9405e26a67fae8b9dd4b39e5a2b408
dorigjohndel <dorigjohndel_bsit@plmun.edu.ph>
2026-09-16 18:49:09 +0800

    Update lab2_decode.php

--- FINALS Lab Exercises #1 JSON/lab2_decode.php
+++ FINALS Lab Exercises #1 JSON/lab2_decode.php
@@ -1,1 +1,10 @@
+<?php
+$jsonString = '{"name":"Dorig, Johndel B.","age":20,"email":"dorigjohndel9@gmail.com"}';
 
+$phpObject = json_decode($jsonString);
+
+$phpArray = json_decode($jsonString, true);
+
+echo "Object: " . $phpObject->name . "<br>";
+echo "Array: " . $phpArray['email'];
+?>
